b4804f9d493460e19695f2a093b2dc6ba23ce0fa
abarhub <abarhub@gmail.com>
2022-10-15 18:09:58 +0200

    Initial commit

---
+++ src/main/java/org/tinydashboard/tinydashboard/TinydashboardApplication.java
@@ -0,0 +1,13 @@
+package org.tinydashboard.tinydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinydashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinydashboardApplication.class, args);
+    }
+
+}
